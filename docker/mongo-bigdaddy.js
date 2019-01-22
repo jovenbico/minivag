a698cd34546218e663dcfcd82c209ebd7e8ecbcd
Joven Frankie Bico <jovenbico@gmail.com>
2019-01-23 01:53:53 +0800

    Update doc- to con-

--- docker/mongo-bigdaddy.js
+++ docker/mongo-bigdaddy.js
@@ -3,7 +3,7 @@ rs.initiate({
    _id : "bigdaddy",
    version : 1,
    members: [
-      { _id: 0, host: "doc-v-mongo1:27017", priority: 1 },
-      { _id: 1, host: "doc-v-mongo2:27017", priority: 0.5 }
+      { _id: 0, host: "con-v-mongo1:27017", priority: 1 },
+      { _id: 1, host: "con-v-mongo2:27017", priority: 0.5 }
    ]
 }, { force: true});
